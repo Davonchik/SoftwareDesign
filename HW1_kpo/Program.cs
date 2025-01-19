75506df4d70ff01201eb9b9b8c0023abb70a67b1
Давид <david@192.168.1.5>
2025-01-19 11:19:59 +0300

    checked

--- HW1_kpo/Program.cs
+++ HW1_kpo/Program.cs
@@ -12,7 +12,7 @@
 
             var factory = new FactoryAF(customers);
 
-            for (int i = 0; i < 1; i++)
+            for (int i = 0; i < 5; i++)
                 factory.AddCar();
 
             Console.WriteLine("Before");
