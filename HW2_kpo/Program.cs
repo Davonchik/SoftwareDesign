689192839db1700583c29d6a93d0bc512024e7cf
Давид <david@MacBook-Pro-David.local>
2025-01-25 19:56:52 +0300

    fix

--- HW2_kpo/Program.cs
+++ HW2_kpo/Program.cs
@@ -7,40 +7,31 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        var carService = new CarStorage();
         var customerStorage = new CustomersStorage();
-
-        var customers = new[]
-        {
-            new Customer(name: "Ivan", legStrength: 10, handStrength: 1),
-            new Customer(name : "Petr", legStrength : 1, handStrength : 10),
-            new Customer(name : "Sidr", legStrength : 1, handStrength : 1),
-            new Customer(name : "Sidr", legStrength : 10, handStrength : 10),
-        };
-
-        foreach (var customer in customers)
-            customerStorage.AddCustomer(customer);
-
+        var hseCarService = new CarShopService(carService, customerStorage);
         var pedalCarFactory = new PedalCarFactory();
         var handCarFactory = new HandCarFactory();
 
-        var carStorage = new CarStorage();
+        customerStorage.AddCustomer(new Customer("Davo", 6, 4));
+        customerStorage.AddCustomer(new Customer("Tigran", 4, 6));
+        customerStorage.AddCustomer(new Customer("Edgar", 6, 6));
+        customerStorage.AddCustomer(new Customer("Gev", 4, 4));
 
-        carStorage.AddCar(pedalCarFactory, new PedalEngineParams(pedalSize: 2));
-        carStorage.AddCar(pedalCarFactory, new PedalEngineParams(pedalSize: 3));
-        carStorage.AddCar(handCarFactory, EmptyEngineParams.Empty);
-        carStorage.AddCar(handCarFactory, EmptyEngineParams.Empty);
-
-        var hseCarShop = new CarShopService(carStorage, customerStorage);
+        carService.AddCar(pedalCarFactory, new PedalEngineParams(4));
+        carService.AddCar(pedalCarFactory, new PedalEngineParams(4));
+        carService.AddCar(handCarFactory, new EmptyEngineParams());
+        carService.AddCar(handCarFactory, new EmptyEngineParams());
 
         Console.WriteLine("=== Покупатели ===");
-        foreach (var customer in customers)
+        foreach (var customer in customerStorage.GetCustomers())
             Console.WriteLine(customer);
 
         Console.WriteLine("\n=== Продажа автомобилей ===\n");
-        hseCarShop.SellCars();
+        hseCarService.SellCars();
 
         Console.WriteLine("=== Покупатели ===");
-        foreach (var customer in customers)
+        foreach (var customer in customerStorage.GetCustomers())
             Console.WriteLine(customer);
     }
 }
